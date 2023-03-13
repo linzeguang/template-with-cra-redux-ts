142de5e60320ae73adfada578064c7eebd15877b
linzeguang <15817289730@163.com>
2023-03-13 16:49:16 +0800

    feat: 新增minireset.css cdn

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,3 @@
-import './global.css'
-
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,7 +1,23 @@
+/**
+ * @Author linzeguang
+ * @Date 2023-03-13 16:22:54
+ * @LastEditTime 2023-03-13 16:47:49
+ * @LastEditors linzeguang
+ * @Description
+ */
+
 import React from 'react'
+import styled from '@emotion/styled'
+
+const Box = styled.div(() => ({
+  width: 200,
+  height: 100,
+  padding: 20,
+  backgroundColor: 'red',
+}))
 
 const About: React.FC = () => {
-  return <div>About</div>
+  return <Box />
 }
 
 export default About
