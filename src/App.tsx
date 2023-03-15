e6d032ba05f6825deb30e5d92730d7b01e74a721
linzeguang <15817289730@163.com>
2023-03-15 10:36:07 +0800

    feat: add mui@5.10.6

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Link } from 'react-router-dom'
+import { Button } from '@mui/material'
 
 import Routing from './routes'
 
 const App: React.FC = () => {
   return (
     <BrowserRouter>
-      <Link to='/'>Home</Link>
-      <Link to='/about'>About</Link>
+      <Link to='/'>
+        <Button>Home</Button>
+      </Link>
+      <Link to='/about'>
+        <Button>About</Button>
+      </Link>
       <Routing />
     </BrowserRouter>
   )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,25 @@
 import React from 'react'
+import { Button } from '@mui/material'
 
 import { template } from '@/api'
 
 const Home: React.FC = () => {
   return (
     <div>
-      <button onClick={() => template.fetchList().then(console.log)}>fetchList</button>
-      <button onClick={() => template.fetchTest().then(console.log)}>fetchTest</button>
-      <button onClick={() => template.fetchTodo().then(console.log).catch(console.log)}>
-        fetchTodo
-      </button>
+      <div>
+        <Button variant='contained' onClick={() => template.fetchList().then(console.log)}>
+          fetchList
+        </Button>
+        <Button variant='contained' onClick={() => template.fetchTest().then(console.log)}>
+          fetchTest
+        </Button>
+        <Button
+          variant='contained'
+          onClick={() => template.fetchTodo().then(console.log).catch(console.log)}
+        >
+          fetchTodo
+        </Button>
+      </div>
     </div>
   )
 }
